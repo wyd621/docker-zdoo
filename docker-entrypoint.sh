5e85f121c3c4002e096fce30f8e900a85d3b1375
zhouyq <zyq916@gmail.com>
2016-09-01 14:40:05 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,76 +1,15 @@
 #!/usr/bin/env bash
 
-# 然之持久化操作
-mkdir -p /data/session
+# link file
+for d in "ranzhi zentao chanzhi"
+do
+# 应用目录有data 目录先删除
+[ -d /app/zdoo/${d}/www/data ] && rm -rf /app/zdoo/${d}/www/data
 
-persist_dirs="www/data config tmp"
-dest_dir=/data/ranzhi
-source_dir=/app/zdoo/ranzhi
-mkdir -p ${dest_dir}
-# 在持久化存储中创建需要的目录
-for d in ${persist_dirs} ; do
-    if [ -d ${dest_dir}/${d} ] ; then
-        rm -rf ${source_dir}/${d}
-    else
-        mkdir -p ${dest_dir}/${d}
-        cp -r ${source_dir}/${d}/* ${dest_dir}/${d}
-        rm -rf ${source_dir}/${d}
-    fi
-    pdir=$(dirname ${source_dir}/${d})
-    ln -s ${dest_dir}/${d} ${pdir}
+# 将持久化目录软连接到应用目录中
+[ -d /mnt/upload/$d ] && ln -s /app/zdoo/${d}/www/data
+
 done
-
-echo "ranzhi success"
-
-# 禅道安装持久化目录
-persist_dirs="www/data config tmp module"
-dest_dir=/data/zentaopms
-source_dir=/app/zdoo/zentao
-mkdir -p ${dest_dir}
-# 在持久化存储中创建需要的目录
-for d in ${persist_dirs} ; do
-    if [ -d ${dest_dir}/${d} ] ; then
-        rm -rf ${source_dir}/${d}
-    else
-        mkdir -p ${dest_dir}/${d}
-        cp -r ${source_dir}/${d}/* ${dest_dir}/${d}
-        rm -rf ${source_dir}/${d}
-    fi
-    pdir=$(dirname ${source_dir}/${d})
-    ln -s ${dest_dir}/${d} ${pdir}
-done
-
-echo "zentao success"
-
-# install.php和upgrade.php
-user_config="${source_dir}/config/my.php"
-install_file="${source_dir}/www/install.php"
-upgrade_file="${source_dir}/www/upgrade.php"
-if [ -f ${user_config} ]; then
-    echo "now check zentao pms need delete install and upgrade"
-    [ -f ${install_file} ] && rm -f ${install_file}
-    [ -f ${upgrade_file} ] && rm -f ${upgrade_file}
-fi
-
-# 蝉知安装持久化目录
-persist_dirs="system/config system/module system/tmp www/data"
-dest_dir=/data/chanzhimall5.6
-source_dir=/app/zdoo/chanzhi
-mkdir -p ${dest_dir}
-# 在持久化存储中创建需要的目录
-for d in ${persist_dirs} ; do
-    if [ -d ${dest_dir}/${d} ] ; then
-        rm -rf ${source_dir}/${d}
-    else
-        mkdir -p ${dest_dir}/${d}
-        cp -r  ${source_dir}/${d}/* ${dest_dir}/${d}
-        rm -rf ${source_dir}/${d}
-    fi
-    pdir=$(dirname ${source_dir}/${d})
-    ln -s ${dest_dir}/${d} ${pdir}
-done
-
-echo "chanzhi success"
 
 # 设置权限
 chown -R www-data.www-data /data/
